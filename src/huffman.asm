de8457039b4caadceb9e7a89f9084c72d0436a72
Muhammad Moiz <m.moiz17110@gmail.com>
2025-05-09 02:11:58 +0500

    Update huffman.asm

--- src/huffman.asm
+++ src/huffman.asm
@@ -1,54 +1,56 @@
-global start
+global _start
 
 section .data
-    sys_write: equ 0x2000004 
-    sys_exit:  equ 0x2000001
-    sys_mmap:  equ 0x20000C5
+    sys_write: equ 1
+    sys_exit:  equ 60
+    sys_mmap:  equ 9
 
     err_insufficient_args: db "Insufficient arguments", 0xA
-    .len: equ $ - err_insufficient_args
+    .err1_len: equ $ - err_insufficient_args
+
     err_invalid_action: db "Invalid action", 0xA
-    .len: equ $ - err_invalid_action
+    .err2_len: equ $ - err_invalid_action
 
     newline: db 0xA
 
 section .text
 
-%include "src/syscalls.asm"
+%include "src/syscalls.asm"   ; Make sure these don't rely on macOS numbers
+%include "src/tree.asm"
+%include "src/encode.asm"
+%include "src/decode.asm"
 
-start:
-    ; Check for neccesary arguments
+_start:
+    ; Check for necessary arguments
     mov rdx, [rsp]
     cmp rdx, 3 
     jl insufficient_args
 
     ; Skip over argc and argv[0]
     add rsp, 0x10
-    ; Retrieve action (either encode or decode)
-    pop rdx
+    pop rdx ; Retrieve action string
 
-    ; Check for encode action
-    cmp byte [rdx], 0x65
+    ; Check if action is "e" (encode)
+    cmp byte [rdx], 'e'
     je .encode
 
-    ; Check for decode action
-    cmp byte [rdx], 0x64
-    jne invalid_action 
- 
-.decode:
-    call decode
-    jmp exit
+    ; Check if action is "d" (decode)
+    cmp byte [rdx], 'd'
+    je .decode
+
+    ; Invalid action
+    jmp invalid_action 
 
 .encode:
     call encode
     jmp exit
 
-%include "src/tree.asm"
-%include "src/encode.asm"
-%include "src/decode.asm"
+.decode:
+    call decode
+    jmp exit
 
 print_line:
-    mov r15, 0x1
+    mov r15, 1
     push r15
     mov r15, newline
     push r15
@@ -56,26 +58,23 @@ print_line:
     add rsp, 0x10
     ret
 
-
-; Print out error for invalid action 
 invalid_action:
     mov rax, sys_write 
-    mov rdi, 1 
+    mov rdi, 1              ; stdout
     mov rsi, err_invalid_action
-    mov rdx, err_invalid_action.len
+    mov rdx, err_invalid_action.err2_len
     syscall
     jmp exit
 
-; Print out error for insufficient arguments 
 insufficient_args:
     mov rax, sys_write 
-    mov rdi, 1 
+    mov rdi, 1              ; stdout
     mov rsi, err_insufficient_args
-    mov rdx, err_insufficient_args.len
+    mov rdx, err_insufficient_args.err1_len
     syscall
     jmp exit
 
 exit:
-    mov rax, sys_exit ; exit
-    mov rdi, 0x0
+    mov rax, sys_exit
+    mov rdi, 0
     syscall
